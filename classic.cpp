407e3bc05f09dcf140f331bc95027c5b9c8f780a
Patrick Swett <prswett@gmail.com>
2018-03-01 14:35:19 -0800

    first commit

---
+++ classic.cpp
@@ -0,0 +1,53 @@
+#include "classic.h"
+
+//constructor for classic movie
+Classic::Classic(char movieGenre, int stock, string director, string title, string majorActor, string releaseDate)
+{
+	//setting variables
+	_movieGenre = movieGenre;
+	_stock = stock;
+	_director = director;
+	_title = title;
+	_majorActor = majorActor;
+	_releaseDate = releaseDate;
+}
+
+//getter for major actor
+string Classic::getMajorActor()
+{
+	return _majorActor;
+}
+
+//getter for release date
+string Classic::getReleaseDate()
+{
+	return _releaseDate;
+}
+
+//gets the full txt represenation of the movie
+string Classic::getFullClassicInfo()
+{
+	string finalResult;
+
+	//use string stream to turn char into a string 
+	stringstream ss;
+	string type;
+	ss << _movieGenre;
+	ss >> type;
+
+	//turn int into a string
+	string stock = to_string(_stock);
+
+	//full movie string
+	finalResult = type + " " + stock + " " + _director + " " + _title + " " + _majorActor + " " + _releaseDate;
+	return finalResult;
+}
+
+// gets the unique movie Id we will use to sort them later in iventory
+//release date then major actor
+string Classic::getUniqueMovieID()
+{
+	string finalResult;
+	finalResult = _releaseDate + " " + _majorActor;
+	return finalResult;
+}
